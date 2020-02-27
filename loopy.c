31dc66115182b3004decf8eb5656346d9da9055d
bglas <bglas@WS-C02YR30PLVCJ.local>
2020-02-27 16:15:03 -0600

    Commit Loopy

---
+++ loopy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Write a program that will out put the following lines using loops:
+ * 1, 2, 3, 4, 5, 6, 7
+ * 3, 8, 13, 18, 23, 28, 33
+ * 20, 14, 8, 2, -4, -10
+ * 19, 27, 35, 43, 51, 59
+ * 
+ * Print those four lines three times, once with each different loop.
+ *  a. for loop
+ *  b. while loop
+ *  c. do-while loop
+ * 
+ */
+int main(void) {
+
+
+
+}
