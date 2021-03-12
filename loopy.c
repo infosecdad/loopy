0b15345acfa9717efa3a5f52990486456df2bbca
Brian Glas <infosecdad@users.noreply.github.com>
2021-03-12 08:33:41 -0600

    Update loopy.c

--- loopy.c
+++ loopy.c
@@ -2,10 +2,10 @@
 
 /**
  * Write a program that will out put the following lines using loops:
- * 1, 2, 3, 4, 5, 6, 7
- * 3, 8, 13, 18, 23, 28, 33
- * 20, 14, 8, 2, -4, -10
- * 19, 27, 35, 43, 51, 59
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14
+ * 3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53
+ * 20, 14, 8, 2, -4, -10, -16, -22, -28
+ * 19, 27, 35, 43, 51, 59, 67, 75
  * 
  * Print those four lines three times, once with each different loop.
  *  a. for loop
